feb5ff1fd6d77b1f39a968f5200858b0e7d25e78
Luis Miguel <inf.lrodriguez@gmail.com>
2019-08-17 03:23:46 -0400

    Google analytics y cacheo de imagenes con workbox

--- src/service-worker.js
+++ src/service-worker.js
@@ -12,10 +12,12 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 // App Shell
 workbox.routing.registerNavigationRoute('/index.html')
 
+workbox.googleAnalytics.initialize()
+
 // La API usa Stale While Revalidate para mayor velocidad
 workbox.routing.registerRoute(/^https?:\/\/www.themealdb.com\/api\/.*/,
-    new workbox.strategies.StaleWhileRevalidate(), 
-        'GET')
+    new workbox.strategies.StaleWhileRevalidate(),
+    'GET')
 
 // Las fuentes van con Cache First y vencen al mes
 workbox.routing.registerRoute(/^https?:\/\/fonts.(?:googleapis|gstatic).com\/(.*)/,
@@ -28,6 +30,17 @@ workbox.routing.registerRoute(/^https?:\/\/fonts.(?:googleapis|gstatic).com\/(.*
         ]
     }),
     'GET')
+workbox.routing.registerRoute(/^https?:\/\/www.themealdb.com\/images\/.*/,
+    new workbox.strategies.CacheFirst({
+        cacheName: 'image-cache',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: 7 * 24 * 60 * 60,
+                maxEntries: 20
+            })
+        ]
+    })
+)
 
 // Todo lo demas usa Network First 
 workbox.routing.registerRoute(/^https?.*/,
